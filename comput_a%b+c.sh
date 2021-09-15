6b7753c7aa7dcef24d328588e232bc805c956aae
rohith kumar gowda h n <rohithkumargowdahn@gmai.com>
2021-09-15 09:35:41 +0530

    compute a%b+c

---
+++ comput_a%b+c.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+echo "--------------------------------Arithmetic Compution And Sorting ----------------------------------"
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
+#COMPUTE ARITHMETIC OPERATION
+resultThree=`echo "scale=2;$firstInput % $secondInput + $thirdInput" | bc`
+echo "Result:" $resultThree
